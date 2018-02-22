d48c822d32d80279a0dc8e0cf66ca9018302ccbd
marianne pearson <marianne.pearson@digital.hmrc.gov.uk>
2018-02-22 17:11:46 +0000

    [APB-2009][JR,MP] Aligning roots to make all posts to details page and use page contents to determine journey

--- app/uk/gov/hmrc/agentepayeregistrationfrontend/models/RegistrationRequest.scala
+++ app/uk/gov/hmrc/agentepayeregistrationfrontend/models/RegistrationRequest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,12 @@ case class RegistrationRequest(
   emailAddress: Option[String],
   address: Address)
 
+case class PageID(pageId: String)
+
 object RegistrationRequest {
   implicit val registrationRequestFormat = Json.format[RegistrationRequest]
+}
+
+object PageID {
+  implicit val pageIdFormat = Json.format[PageID]
 }
