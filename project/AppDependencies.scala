86424e03efb99bc3450fb28df44081fae2a36dd2
Ahmad Naeem <67148921+AhmadNaeemQA@users.noreply.github.com>
2024-03-28 10:06:08 +0000

    DL-12421 play 3.0 migration

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,25 +2,25 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.22.0"
-  val mongoVersion = "1.3.0"
+  val bootstrapVersion = "8.4.0"
+  val mongoVersion = "1.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28" % "8.5.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVersion,
-    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.13.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "9.3.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.0.0",
     "uk.gov.hmrc"       %% "emailaddress"               % "3.8.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % bootstrapVersion,
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % bootstrapVersion,
     "org.scalatestplus"    %% "scalacheck-1-15"          % "3.2.11.0",
     "org.mockito"          %% "mockito-scala"            % "1.17.27",
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-28"  % mongoVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % mongoVersion,
     "com.vladsch.flexmark" %  "flexmark-all"             % "0.62.2" // Required to stay at this version - see https://github.com/scalatest/scalatest/issues/1736
-  ).map(_ % "test, it")
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
