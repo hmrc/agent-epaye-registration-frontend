8b4d7a5779a23952eb915b9801440fb0e829cf77
Kwame Frimpong-Manso <171707914+kwame-f-manso-acc@users.noreply.github.com>
2025-04-01 12:28:05 +0100

    DL-15855: upgraded dependecies (#114)
    * DL-15855: upgraded dependecies
    
    * DL-15855: addressed bobby violations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.7.0"
-  val mongoVersion     = "2.4.0"
+  val bootstrapVersion = "9.11.0"
+  val mongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0",
