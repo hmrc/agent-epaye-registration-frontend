44393abc860337962abd28f891af54bdacb5fb09
DivyaManimaran_hmrc <179695051+DivyaManimaran_hmrc@users.noreply.github.com>
2025-07-15 16:23:53 +0100

    DL-16924: Rectify bobby violations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "9.16.0"
   val mongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.7.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0"
